a499dbce7dbf4302fb36d42c2e35d18a5b27c7c2
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-02-09 01:08:33 +0900

    문제풀이 Main 템플릿화

--- JAVA/src/Main.java
+++ JAVA/src/Main.java
@@ -1,43 +1,38 @@
+/*
+- 입력
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+- 한 줄 입력 받기
+String input = br.readLine();
+- 입력받은 값 String -> int(숫자) 변경
+int N = Integer.parseInt(input);
+- 한 줄 입력받은 값 공백 기준으로 배열 담기
+String[] line = br.readLine().split(" ");
+
+- 출력
+BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+bw.write()
+bw.newLine(); // 줄바꿈
+bw.flush(); // 버퍼 내용 출력
+bw.close(); // BufferedWriter 닫기
+br.close(); // BufferedReader 닫기
+*/
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int C = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        String[] line = br.readLine().split(" ");
 
-        for (int c = 0; c < C; c++) {
-            String[] line = br.readLine().split(" ");
-
-            int N = Integer.parseInt(line[0]);
-            int[] scores = new int[N];
-
-            for (int i = 0; i < N; i++) {
-                scores[i] = Integer.parseInt(line[i + 1]);
-            }
-
-            double sum = 0;
-            for (int score : scores) {
-                sum += score;
-            }
-            double avg = sum / N;
-
-            int count = 0;
-            for (int score : scores) {
-                if (score > avg) {
-                    count++;
-                }
-            }
-
-            double percentage = (count / (double) N) * 100;
-            System.out.printf("%.3f%%\n", percentage);
-        }
-
+        bw.flush();
+        bw.close();
         br.close();
     }
 }
-
 
 //import java.io.*;
 //import java.util.*;
@@ -47,16 +42,12 @@ public class Main {
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 //
-//        // 예제 입력 받기
-//        String input = br.readLine(); // 한 줄 입력 받기
-//        int num = Integer.parseInt(input); // 숫자로 변환
+//        int N = Integer.parseInt(br.readLine());
+//        String[] line = br.readLine().split(" ");
 //
-//        // 예제 출력
-//        bw.write("입력값: " + num + "\n");
-//
-//        // 자원 닫기 (필수)
-//        br.close();
-//        bw.flush(); // 남아 있는 출력 데이터를 모두 출력
+//        bw.flush();
 //        bw.close();
+//        br.close();
 //    }
 //}
+
