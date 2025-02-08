1afcf4037bc4715a464eec071cfa61185820ee1b
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-02-08 23:43:15 +0900

    BOJ_4344_B1

--- JAVA/src/수학/BOJ_4344_B1.java
+++ JAVA/src/수학/BOJ_4344_B1.java
@@ -1,7 +1,8 @@
-import java.io.*;
-import java.util.*;
+package 수학;
 
-public class Main {
+import java.io.*;
+
+public class BOJ_4344_B1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -38,25 +39,13 @@ public class Main {
     }
 }
 
+//5
+//5 50 50 70 80 100
+//7 100 95 90 80 70 60 50
+//3 70 90 80
+//3 70 90 81
+//9 100 99 98 97 96 95 94 93 91
 
-//import java.io.*;
-//import java.util.*;
-//
-//public class Main {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//
-//        // 예제 입력 받기
-//        String input = br.readLine(); // 한 줄 입력 받기
-//        int num = Integer.parseInt(input); // 숫자로 변환
-//
-//        // 예제 출력
-//        bw.write("입력값: " + num + "\n");
-//
-//        // 자원 닫기 (필수)
-//        br.close();
-//        bw.flush(); // 남아 있는 출력 데이터를 모두 출력
-//        bw.close();
-//    }
-//}
+
+
+
