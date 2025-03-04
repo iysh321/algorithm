ef00c8d95ec06c30303719c7def661a8f9116415
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-03-04 15:29:32 +0900

    BOJ_13305_S3.java

---
+++ JAVA/src/그리디/BOJ_13305_S3.java
@@ -0,0 +1,30 @@
+package 그리디;
+
+import java.io.*;
+
+public class BOJ_13305_S3 {
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            int N = Integer.parseInt(br.readLine());
+            String[] km = br.readLine().split(" ");
+            String[] price = br.readLine().split(" ");
+
+            long result = Long.parseLong(km[0]) * Long.parseLong(price[0]);
+            long min = Long.parseLong(price[0]);
+
+            for (int i = 1; i < N - 1; i++) {
+                min = Math.min(min, Long.parseLong(price[i]));
+                result += min * Long.parseLong(km[i]);
+            }
+
+            bw.write(String.valueOf(result));
+
+            bw.flush();
+            bw.close();
+            br.close();
+        }
+    }
+}
